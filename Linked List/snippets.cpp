2798d1b93f92774350fd8072d3a3de1fd90e2182
chalini99 <chalinim9094@gmail.com>
2026-07-06 00:38:14 +0530

    done

---
+++ Linked List/snippets.cpp
@@ -0,0 +1,48 @@
+
+bool search(ListNode* head, int key)
+{
+    ListNode* temp = head;
+    while (temp != nullptr) {
+        if (temp->val == key) {
+            return true; 
+        }
+        temp = temp->next;
+    }
+    return false; 
+}
+
+int countNodes(ListNode* head)
+{
+   int count=0;
+   ListNode* temp =head;
+   while(temp!=NULL){
+      count++;
+      temp=temp->next;
+   }
+   return count;
+}
+
+int findMax(ListNode* head)
+{
+   int maxi = head->val;
+   ListNode* temp= head;
+   while(temp!=NULL){
+       if (temp->val > maxi){
+        maxi = temp->val;
+       }
+        
+   }
+   return maxi;
+}
+
+
+int sum(ListNode* head)
+{
+    int sum=0;
+    ListNode * temp= head;
+    while(temp!=NULL){
+        sum+=temp->val;
+        temp= temp->next;
+    }
+    return sum;
+}
